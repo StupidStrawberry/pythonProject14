d889bd5519aac57c36dec14d9df3267dcb562263
TolyaHuligan <12345ghfuf@gmail.com>
2025-02-15 15:38:56 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel
+from models import Polzovatel
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read():
+    with open("index.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+class CalculateRequest(BaseModel):
+    num1: int
+    num2: int
+
+
+User = Polzovatel(name="John Doe", id=1)
+
+
+@app.post("/calculate")
+async def calculate(data: CalculateRequest):
+    result = data.num1 + data.num2
+    return {"result": result}
+
+
+@app.get("/users")
+async def det_user():
+    return User
